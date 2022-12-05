12237f06fa040a12bfdd3ad6d19994881d26b46d
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-06 02:16:57 +0500

    feat: подключил шрифты

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { cofigureStore } from "./store/index";
 import { Root } from "./Root";
 import './vendor/normalize.scss';
+import './vendor/fonts.scss';
 import './index.scss';
 
 const store = cofigureStore();
